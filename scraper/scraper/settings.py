dfff8121993cfe41913ee2243998725280c84766
ggengelmo <g.garcia2@udc.es>
2025-10-04 00:53:10 +0200

    feat: recorrer todas las firmas que contiene preguntas/respuesta y descargar JSON

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -12,7 +12,8 @@ BOT_NAME = "scraper"
 SPIDER_MODULES = ["scraper.spiders"]
 NEWSPIDER_MODULE = "scraper.spiders"
 
-
+CONCURRENT_REQUESTS = 4
+DOWNLOAD_DELAY = 0.8
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "scraper (+http://www.yourdomain.com)"
 
